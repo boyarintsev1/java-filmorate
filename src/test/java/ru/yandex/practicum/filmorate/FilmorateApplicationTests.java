9bd20dd567785fc3cb2f1a8273c339094f779629
boyarintsev1 <126398007+boyarintsev1@users.noreply.github.com>
2023-08-29 21:52:55 +0300

    Update FilmorateApplicationTests.java

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,10 +1,7 @@
 package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.controller.FilmController;
-import ru.yandex.practicum.filmorate.controller.FilmControllerTest;
 
 @SpringBootTest
 class FilmorateApplicationTests {
